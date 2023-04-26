6958932819badc839523e7cf4388d776262136dc
stan5315 <nwoforstanley@gmail.com>
2023-04-26 11:45:21 -0700

    update

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,33 +1,27 @@
 #include "sort.h"
+
 /**
- * shell_sort - sorts shelly like
- * @array: array to take in
+ * shell_sort - Shell sort - Knuth Sequence
+ * @array: array to sort
  * @size: size of array
- *
  */
 void shell_sort(int *array, size_t size)
 {
-	size_t gap = 1, i, j;
-	int tmp;
+	int gap = 1, i, j, tmp;
 
-	if (array == NULL || size < 2)
-		return;
-
-	while (gap < size / 3)
-		gap = (gap * 3) + 1;
-
-	while (gap)
+	while (gap < ((int)(size)))
+		gap = (3 * gap) + 1;
+	for (gap = (gap - 1) / 3; gap > 0; gap = (gap - 1) / 3)
 	{
-		for (i = gap; i < size; i++)
+		for (i = gap; i < (int)size; i += 1)
 		{
 			tmp = array[i];
+
 			for (j = i; j >= gap && array[j - gap] > tmp; j -= gap)
-			{
 				array[j] = array[j - gap];
-			}
+
 			array[j] = tmp;
 		}
 		print_array(array, size);
-		gap /= 3;
 	}
 }
